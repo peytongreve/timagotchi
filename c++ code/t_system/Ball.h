1613dfbb79f84821cb9220e62c2dbfc6fe6e2c33
Julian manyika <julian@MacBook-Pro-73.local>
2020-04-28 21:50:10 -0700

    week 3 report template

---
+++ c++ code/t_system/Ball.h
@@ -0,0 +1,27 @@
+#ifndef Ball_h
+#define Ball_h
+#include <SPI.h>
+#include <TFT_eSPI.h>
+
+class Ball {
+    float x_pos;
+    float Y_POS;
+    float x_vel;
+    float x_accel;
+    int BALL_CLR;
+    int BKGND_CLR;
+    int MASS;
+    int RADIUS;
+    int LEFT_LIMIT;
+    int RIGHT_LIMIT;
+    float K_SPRING;
+    int DT;
+    TFT_eSPI tft;
+
+    public:
+        void step();
+        void reset();
+    private:
+        void moveBall();
+}
+#endif
